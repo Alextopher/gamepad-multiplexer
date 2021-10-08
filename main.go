58b51956dca8e2790762421b74ba386b89576237
stew <rtstewy@gmail.com>
2021-10-08 00:27:10 -0400

    Get joystick buttons

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+  "log"
 	"runtime"
 
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
+
+func joystick() {
+  // clear state
+	joy := glfw.Joystick(glfw.Joystick1)
+  log.Println(joy)
+
+	axes := joy.GetAxes()
+	buttons := joy.GetButtons()
+
+  log.Println(axes)
+  log.Println(buttons)
+}
 
 func init() {
 	// This is needed to arrange that main() runs on main thread.
@@ -19,16 +32,5 @@ func main() {
 	}
 	defer glfw.Terminate()
 
-	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	window.MakeContextCurrent()
-
-	for !window.ShouldClose() {
-		// Do OpenGL stuff.
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
+  joystick()
 }
